59e4a1295ce85a69750016ca731a953afecb27eb
Richard Kruse <richardkruse3rd@gmail.com>
2022-10-13 17:30:43 +0900

    L5: Built database models & connected them to adminsite

--- crm1/accounts/admin.py
+++ crm1/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass 
+
+admin.site.register(Customer, AuthorAdmin)
+admin.site.register(Product, AuthorAdmin)
+admin.site.register(Order, AuthorAdmin)
