098570ebba438fdb3239cd940a5f1aa71aba1d58
Richard Kruse <richardkruse3rd@gmail.com>
2022-10-15 15:53:36 +0900

    PT9: Dynamic URL routing & templates update

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -27,7 +27,6 @@ class Product(models.Model):
     name = models.CharField(max_length =200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length =200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length =200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     tags =models.ManyToManyField(Tag)
 
